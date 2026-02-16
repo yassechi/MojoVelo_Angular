6835085fd39ad180fce69ec34aed5d580d8232fa
yassechi <yassechi@gmail.com>
2026-02-16 23:02:44 +0100

    Change environnement for Azure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,19 @@
 export const environment = {
   urls: {
-    coreBase: 'https://localhost:7000',
-    coreApi: 'https://localhost:7000/api',
-    legacyApi: 'http://localhost:5000/api',
+    coreBase: 'https://mojoapi-cqcdfzgsgebcc9d5.westeurope-01.azurewebsites.net',
+    coreApi: 'https://mojoapi-cqcdfzgsgebcc9d5.westeurope-01.azurewebsites.net/api',
     cmsApi: 'https://mojovelo.be/wp-json/wp/v2',
   },
 };
+
+
+// export const environment = {
+//   urls: {
+//     // coreBase: 'https://localhost:7000',
+//     coreBase: 'mojoapi-cqcdfzgsgebcc9d5.westeurope-01.azurewebsites.net',
+//     coreApi: 'mojoapi-cqcdfzgsgebcc9d5.westeurope-01.azurewebsites.net/api',
+//     // legacyApi: 'http://localhost:5000/api',
+//     cmsApi: 'https://mojovelo.be/wp-json/wp/v2',
+//   },
+// };
+
